89f4977e7832de555cb490d1dc4e0221507b13f9
Glen Whitney <glen@nobody@nowhere.net>
2022-07-20 02:57:46 +0000

    doc: Move an incorrectly placed comment

--- complex/negate.mjs
+++ complex/negate.mjs
@@ -1,7 +1,7 @@
 import {numComplex} from './Complex.mjs'
 export const negate = {
-   /* need a "base case" to avoid infinite self-reference */
    Complex: [['self'], ref => z => {
+      /* need a "base case" to avoid infinite self-reference */
       if (numComplex(z)) return {re: -z.re, im: -z.im}
       return {re: ref.self(z.re), im: ref.self(z.im)}
    }]
