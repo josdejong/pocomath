353f65eaae9ff5b80002d9d6a196f3628313ac4c
Jos de Jong <wjosdejong@gmail.com>
2022-12-21 16:55:20 +0100

    feat: add an isReal(number) implementation

--- src/complex/isReal.ts
+++ src/complex/isReal.ts
@@ -1,13 +1,19 @@
 import { Add, Equal } from '../interfaces/arithmetic.js'
 import { Complex } from './interfaces.js'
 
-export type IsReal<T> = (z: Complex<T>) => boolean
+export type IsReal<T> = (z: T) => boolean
+export type IsRealNumber = IsReal<number>
+export type IsRealComplex<T> = IsReal<Complex<T>>
 
 export const isReal = {
-   infer: function <T>({ equal, add }: {
+   'infer:1': function <T>(): IsRealNumber {
+      return (z) => true
+   },
+
+   'infer:2': function <T>({ equal, add }: {
       equal: Equal<T>,
       add: Add<T>
-   }): IsReal<T> {
+   }): IsRealComplex<T> {
       return z => equal(z.re, add(z.re, z.im))
    }
 }
