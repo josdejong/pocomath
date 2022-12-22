aaaa428789c0dd452533ebbc6d7ec2182091db0b
Jos de Jong <wjosdejong@gmail.com>
2022-12-22 10:23:47 +0100

    Move the different syntax example from isReal to absquare

--- src/complex/isReal.ts
+++ src/complex/isReal.ts
@@ -5,51 +5,15 @@ export type IsReal<T> = (z: T) => boolean
 export type IsRealNumber = IsReal<number>
 export type IsRealComplex<T> = IsReal<Complex<T>>
 
-export type AddDep<T> = {
-   add: Add<T>
-}
-
-export type EqualDep<T> = {
-   equal: Equal<T>
-}
-
-// we could bundle dependencies in logical groups
-// not sure if this is something we should want 
-export type ArithmeticDeps<T> = AddDep<T> & EqualDep<T> // & ...
-
 export const isReal = {
-   'infer:0': function <T>(): IsRealNumber {
+   'infer:1': function <T>(): IsRealNumber {
       return (z) => true
    },
 
-   // approach 1 (name 3x)
-   'infer:1': function <T>({ equal, add }: {
+   'infer:2': function <T>({ equal, add }: {
       equal: Equal<T>,
       add: Add<T>
    }): IsRealComplex<T> {
-      return z => equal(z.re, add(z.re, z.im))
-   },
-
-   // approach 2 (name 2x, but no destructuring, have to write dep.equal and dep.add)
-   'infer:2': function <T>(dep: {
-      equal: Equal<T>,
-      add: Add<T>
-   }): IsRealComplex<T> {
-      return z => dep.equal(z.re, dep.add(z.re, z.im))
-   },
-
-   // approach 3 (name 2x)
-   'infer:3': function <T>({ equal, add }: AddDep<T> & EqualDep<T>): IsRealComplex<T> {
-      return z => equal(z.re, add(z.re, z.im))
-   },
-
-   // approach 4 (name 1x, but no destructuring, have to write dep.equal and dep.add)
-   'infer:4': function <T>(dep: AddDep<T> & EqualDep<T>): IsRealComplex<T> {
-      return z => dep.equal(z.re, dep.add(z.re, z.im))
-   },
-
-   // approach 5 (name 2x, injecting a bunch of deps in one go)
-   'infer:5': function <T>({ equal, add }: ArithmeticDeps<T>): IsRealComplex<T> {
       return z => equal(z.re, add(z.re, z.im))
    }
 }
--- src/generic/absquare.ts
+++ src/generic/absquare.ts
@@ -2,11 +2,41 @@ import { Abs, Square } from "../interfaces/arithmetic"
 
 export type AbsSquare<T> = (x: T) => T
 
+export type AbsDep<T> = {
+   abs: Abs<T>
+}
+export type SquareDep<T> = {
+   square: Square<T>
+}
+
+// we could bundle dependencies in logical groups
+// not sure if this is something we should want 
+export type ArithmeticDeps<T> = AbsDep<T> & SquareDep<T> // & ...
+
 export const absquare = {
-   'infer': function <T>({ square, abs }: {
+   // approach 1: destructuring and an object with dependencies
+   'infer:1': function <T>({ square, abs }: {
       square: Square<T>,
       abs: Abs<T>
    }): AbsSquare<T> {
+      return (t) => square(abs(t))
+   },
+
+   // approach 2: no destructuring
+   'infer:2': function <T>(dep: {
+      square: Square<T>,
+      abs: Abs<T>
+   }): AbsSquare<T> {
+      return (t) => dep.square(dep.abs(t))
+   },
+
+   // approach 3: use a union of special Dependency types
+   'infer:3': function <T>({ square, abs }: AbsDep<T> & SquareDep<T>): AbsSquare<T> {
+      return (t) => square(abs(t))
+   },
+
+   // approach 4: use a bundle of special Dependency types
+   'infer:4': function <T>({ square, abs }: ArithmeticDeps<T>): AbsSquare<T> {
       return (t) => square(abs(t))
    }
 }
