3216fb96fea27508eadb456dd31ad620109cd856
Jos de Jong <wjosdejong@gmail.com>
2022-12-22 13:48:32 +0100

    give the ComplexFn types more meaningful names

--- src/complex/complex.ts
+++ src/complex/complex.ts
@@ -1,17 +1,17 @@
 import { Zero } from '../interfaces/arithmetic.js'
 import { Complex } from './interfaces.js'
 
-export type ComplexFn1<T> = (x: T) => Complex<T>
-export type ComplexFn2<T> = (re: T, im: T) => Complex<T>
-export type ComplexFn<T> = ComplexFn1<T> & ComplexFn2<T>
+export type ComplexUnary<T> = (x: T) => Complex<T>
+export type ComplexBinary<T> = (re: T, im: T) => Complex<T>
+export type ComplexFn<T> = ComplexUnary<T> & ComplexBinary<T>
 
 export const complex = {
    'infer:1': function <T>({ zero }: {
       zero: Zero<T>
-   }): ComplexFn1<T> {
+   }): ComplexUnary<T> {
       return (x) => ({ re: x, im: zero(x) })
    },
-   'infer:2': function <T>(): ComplexFn2<T> {
+   'infer:2': function <T>(): ComplexBinary<T> {
       return (re, im) => ({ re, im })
    }
 }
